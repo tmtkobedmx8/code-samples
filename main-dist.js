4db2151daaf57e7585629c67c44c5ba6326b4277
tmtkobedmx8 <thierrymwamba@yahoo.fr>
2018-05-29 22:55:44 -0400

    changed picture

--- main-dist.js
+++
@@ -1,1 +0,0 @@
-$(window).on("load",function(){$(".header-title").addClass("animated bounceInDown"),$(".header-title").css("display","block"),$(".header-title").on("click",function(){$(".header-title").addClass("animated fadeOut")}),$(".main").addClass("animated slideInUp"),$(".main").css("display","block")});
